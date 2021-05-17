5188efdcd74d94c21fd5ab39b501806413e08737
EmmanuelKumah <mailkumah@gmail.com>
2021-05-17 19:14:04 +0000

    Feature: Add tourist site markers on map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   GoogleMap,
   Marker,
   withScriptjs,
   withGoogleMap,
+  InfoWindow,
 } from "react-google-maps";
 import { tours } from "./data/toursData";
 import uuid from "react-uuid";
 
 function Map() {
+  //store selected site data
+  const [selectedTourSite, setSelectedTourSite] = useState(null);
+  console.log("the selected site is", selectedTourSite);
   return (
     <GoogleMap
       defaultZoom={10}
@@ -21,9 +25,22 @@ function Map() {
           <Marker
             key={uuid()}
             position={{ lat: site.cordinates[0], lng: site.cordinates[1] }}
+            onClick={() => setSelectedTourSite(site)}
           />
         );
       })}
+      {/* show info window if a site has been selected */}
+      {selectedTourSite ? (
+        <InfoWindow
+          position={{
+            lat: selectedTourSite.cordinates[0],
+            lng: selectedTourSite.cordinates[1],
+          }}
+          onCloseClick={() => setSelectedTourSite(null)}
+        >
+          <div>Selected tour site</div>
+        </InfoWindow>
+      ) : null}
     </GoogleMap>
   );
 }
--- src/components/data/toursData.js
+++ src/components/data/toursData.js
@@ -1,19 +1,44 @@
 export const tours = [
   {
     name: "Silver Lounge, Accra Mall",
-    desc: "",
+    desc:
+      "The Accra Mall is a shopping centre in Accra, Ghana, located on the Spintex road adjacent to the Tema Motorway. The mall was commissioned on 4 July 2008. It is owned by Atterbury Property Development, Sanlam and the Owusu-Akyaw family",
     cordinates: [5.6219489078821, -0.17333022427134],
+    img: "/images/accramall.jpg",
   },
   {
     name: "Kwame Nkrumah Memorial Park",
     desc:
       "The Kwame Nkrumah Memorial Park and Museum is a beautiful tribute to a great man.This site is the final resting place of Ghana's 1st President.The museum houses some of Nkrumahs's personal items and chronicles Ghana's move from colonial rule to independence",
     cordinates: [5.545, -0.2031],
+    img: "/images/kwameNkumah.jpg",
   },
   {
     name: "Accra international conference centre",
     desc:
       "The Accra International Conference Centre is an events venue in Accra, Ghana. Other venues include the Ghana Trade Fair Center and the National Theatre, but the Conference Centre is the most popular ...",
     cordinates: [5.5626, -0.2012],
+    img: "/images/aicc.jpg",
+  },
+  {
+    name: "Labadi Beach",
+    desc:
+      "Labadi Beach or more properly known as La Pleasure Beach is the busiest beach on Ghana's coast. It is one of Accra's beaches and is maintained by the local hotels.",
+    cordinates: [5.562204, -0.13853],
+    img: "/images/labadi.jpg",
+  },
+  {
+    name: "Black Star Square",
+    desc:
+      "The Independence Arch in Accra, Ghana, is part of the Independence Square which contains monuments to Ghana's independence struggle, including the Independence Arch, Black Star Gate, and the Liberation Day Monument",
+    cordinates: [5.5476, -0.1926],
+    img: "/images/black-star.jpg",
+  },
+  {
+    name: "Osu Castle",
+    desc:
+      "Osu Castle, also known as Fort Christiansborg or simply the Castle, is a castle located in Osu, Accra, Ghana on the coast of the Atlantic Ocean's Gulf of Guinea",
+    cordinates: [5.5415, -0.1762],
+    img: "/images/castle.png",
   },
 ];
